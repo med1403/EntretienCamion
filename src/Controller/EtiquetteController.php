855c44ec2cd132141ebd6344e51c00c460988a2c
Fode Kerfala Camara <kerfalafodec@gmail.com>
2024-06-19 19:36:31 +0200

    Mise à jour apportée par le binome 14 et 7 en ajoutant la fonctionnalité d'impression dans l'etiquette

--- src/Controller/EtiquetteController.php
+++ src/Controller/EtiquetteController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use App\Entity\Etiquette;
 use App\Form\EtiquetteType;
 use App\Repository\EtiquetteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+//Importation de la bibliothèque pour générer  
+//le pdf pour faire l'impression
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -86,6 +90,59 @@ class EtiquetteController extends AbstractController
 
         return $this->render('etiquette/list.html.twig', [
             'etiquettes' => $etiquettes,
+        ]);
+    }
+
+    //Implementation de la fonction pour faire le PDF ensuite faire l'impression
+    #[Route('/imprimer/{id}', name: 'etiquette_imprimer')]
+    public function imprimerEtiquette(EtiquetteRepository $etiquetteRepository, int $id): Response
+    {
+        $etiquette = $etiquetteRepository->find($id);//Je récupère l'id de l'étiquette passer
+        //Ceci est codé par SOLO457: 19/06/2024 16:58
+        if (!$etiquette) {
+            //Fais des instructions en rapport avec le code
+            //On lève une exception
+            throw $this->createNotFoundException('ERREUR: L\'étiquette fournie est innexistante dans le système.');
+        }
+
+        // Configuration de la bibliothèque Dompdf 
+        $pdfOptions = new Options();//Je crée une instance de la Options
+        $pdfOptions->set('defaultFont', 'Arial');//Je fais la configuration par defaut avec le font
+
+        //Je crée une instance de la classe DomPdfn et je lui passe l'objet $pdfOptions
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Je fais appelle au fichier HTML qui dois afficher la sortie dans la vue
+        $html = $this->renderView('etiquette/imprimer.html.twig', [
+            'etiquette' => $etiquette,//On retourne l'étiquette spécifique  mprimer
+        ]);
+
+        // On charge le fichier HTML dans le Dompdf
+        $dompdf->loadHtml($html);
+
+        // Je donne l'orientation et la taille du papier à imprimer
+        $dompdf->setPaper('A4', 'portrait');
+
+        // J rends le HTML en PDF afin d'enlever le format HTML 
+        $dompdf->render();
+
+        /*
+        Ici, stream() envoie le PDF généré directement vers le navigateur.
+        L'option "Attachment" => false Va indiquer que le PDF doit être affiché dans le navigateur
+        plutôt que téléchargé en tant que pièce jointe.
+        */
+        $dompdf->stream("etiquette_SOLO457.pdf", [
+            "Attachment" => false
+        ]);
+        /*
+         Cette instruction return envoie une réponse HTTP indiquant
+         que le type de contenu est application/pdf.
+         Cela va me permettre d'informer le navigateur,
+         sur la manière de traiter la réponse.
+        il s'attend à recevoir un fichier PDF.
+         */
+        return new Response('', 200, [
+            'Content-Type' => 'application/pdf',
         ]);
     }
 }
