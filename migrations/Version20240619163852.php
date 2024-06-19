52c4dc06d50e0fa7772ee62bf779ed32e7b8e8d3
diallo315 <diallomika866@gmail.com>
2024-06-19 17:00:02 +0000

    Verification_BON

---
+++ migrations/Version20240619163852.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240619163852 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE controleur (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, adresse VARCHAR(255) NOT NULL, numero_de_tel VARCHAR(255) NOT NULL, adresse_email VARCHAR(255) NOT NULL, numero_de_badge VARCHAR(255) NOT NULL, date_de_naissance DATE NOT NULL, categories_permis VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP TABLE user');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, roles JSON NOT NULL, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, nom VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prenom VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, email VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, telephone VARCHAR(20) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, photo VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, type_admin VARCHAR(50) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE controleur');
+    }
+}
--- src/Controller/ControleurController.php
+++ src/Controller/ControleurController.php
@@ -3,79 +3,79 @@
 namespace App\Controller;
 
 use App\Entity\Controleur;
-use App\Form\ControleurType;
+use App\Form\Controleur1Type;
 use App\Repository\ControleurRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/truck/maintenance')]
-class TruckMaintenanceController extends AbstractController
+#[Route('/controleur')]
+class ControleurController extends AbstractController
 {
-    #[Route('/', name: 'truck_maintenance_index', methods: ['GET'])]
+    #[Route('/', name: 'app_controleur_index', methods: ['GET'])]
     public function index(ControleurRepository $controleurRepository): Response
     {
-        return $this->render('truck_maintenance/index.html.twig', [
+        return $this->render('controleur/index.html.twig', [
             'controleurs' => $controleurRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'truck_maintenance_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_controleur_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $controleur = new Controleur();
-        $form = $this->createForm(ControleurType::class, $controleur);
+        $form = $this->createForm(Controleur1Type::class, $controleur);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($controleur);
             $entityManager->flush();
 
-            return $this->redirectToRoute('truck_maintenance_index');
+            return $this->redirectToRoute('app_controleur_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('truck_maintenance/new.html.twig', [
+        return $this->render('controleur/new.html.twig', [
             'controleur' => $controleur,
-            'form' => $form->createView(),
+            'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'truck_maintenance_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_controleur_show', methods: ['GET'])]
     public function show(Controleur $controleur): Response
     {
-        return $this->render('truck_maintenance/show.html.twig', [
+        return $this->render('controleur/show.html.twig', [
             'controleur' => $controleur,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'truck_maintenance_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_controleur_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Controleur $controleur, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(ControleurType::class, $controleur);
+        $form = $this->createForm(Controleur1Type::class, $controleur);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('truck_maintenance_index');
+            return $this->redirectToRoute('app_controleur_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('truck_maintenance/edit.html.twig', [
+        return $this->render('controleur/edit.html.twig', [
             'controleur' => $controleur,
-            'form' => $form->createView(),
+            'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'truck_maintenance_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_controleur_delete', methods: ['POST'])]
     public function delete(Request $request, Controleur $controleur, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$controleur->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete'.$controleur->getId(), $request->getPayload()->get('_token'))) {
             $entityManager->remove($controleur);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('truck_maintenance_index');
+        return $this->redirectToRoute('app_controleur_index', [], Response::HTTP_SEE_OTHER);
     }
 }
